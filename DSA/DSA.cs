6cd8fb2918824ca548abdad240c4b856b6d134a5
Ruslan <Ruslan@DESKTOP-2C7OVNH>
2024-11-21 21:11:39 +0200

    Add project files.

---
+++ DSA/DSA.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text.Json;
+using System.IO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DSA
+{
+    public class DSA
+    {
+        private DSAParameters _privateKey;
+        private DSAParameters _publicKey;
+
+        public void GenerateKeys()
+        {
+            using (DSACryptoServiceProvider dsa = new DSACryptoServiceProvider())
+            {
+                _privateKey = dsa.ExportParameters(true);  // Приватний ключ
+                _publicKey = dsa.ExportParameters(false); // Публічний ключ
+                Console.WriteLine($"\nPublic: {_publicKey}\n");
+                Console.WriteLine($"\nPrivate: {_privateKey}\n");
+            }
+        }
+
+        public DSA() => GenerateKeys();
+
+        public void ExportKeys(string privateKeyPath, string publicKeyPath)
+        {
+            File.WriteAllText(privateKeyPath, JsonSerializer.Serialize(_privateKey));
+            File.WriteAllText(publicKeyPath, JsonSerializer.Serialize(_publicKey));
+            Console.WriteLine("Exported keys.");
+        }
+
+        public void ImportKeys(string privateKeyPath, string publicKeyPath)
+        {
+            if (File.Exists(privateKeyPath))
+            {
+                _privateKey = JsonSerializer.Deserialize<DSAParameters>(File.ReadAllText(privateKeyPath));
+            }
+            if (File.Exists(publicKeyPath))
+            {
+                _publicKey = JsonSerializer.Deserialize<DSAParameters>(File.ReadAllText(publicKeyPath));
+            }
+            Console.WriteLine("Imported keys.");
+            Console.WriteLine($"\nPublic: {_publicKey}\n");
+            Console.WriteLine($"\nPrivate: {_privateKey}\n");
+        }
+
+        private byte[] ComputeHash(Stream dataStream)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                return sha1.ComputeHash(dataStream);
+            }
+        }
+
+        public byte[] SignHash(byte[] hash)
+        {
+            using (DSACryptoServiceProvider dsa = new DSACryptoServiceProvider())
+            {
+                dsa.ImportParameters(_privateKey);
+                return dsa.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"));
+            }
+        }
+
+        public byte[] SignString(string input)
+        {  
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] dataBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hash =  sha1.ComputeHash(dataBytes);
+                return SignHash(hash);
+            }
+        }
+
+        public bool VerifyHash(byte[] hash, byte[] signature)
+        {
+            using (DSACryptoServiceProvider dsa = new DSACryptoServiceProvider())
+            {
+                dsa.ImportParameters(_publicKey);
+                return dsa.VerifyHash(hash, CryptoConfig.MapNameToOID("SHA1"), signature);
+            }
+        }
+
+        public byte[] SignFile(string filePath)
+        {
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                byte[] hash = ComputeHash(fileStream);
+                return SignHash(hash);
+            }
+        }
+
+        public bool VerifyFile(string filePath, byte[] signature)
+        {
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                byte[] hash = ComputeHash(fileStream);
+                return VerifyHash(hash, signature);
+            }
+        }
+
+        public void SaveSignature(string filePath, byte[] signature)
+        {
+            File.WriteAllText(filePath, BitConverter.ToString(signature).Replace("-", ""));
+        }
+
+        public byte[] LoadSignature(string filePath)
+        {
+            string hex = File.ReadAllText(filePath);
+            int length = hex.Length / 2;
+            byte[] signature = new byte[length];
+            for (int i = 0; i < length; i++)
+            {
+                signature[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return signature;
+        }
+    }
+}
---
+++ DSA/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.Security.Cryptography;
+
+namespace DSA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DSA dsa = new DSA();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\nМеню:");
+                Console.WriteLine("1. Generate new keys");
+                Console.WriteLine("2. Export keys");
+                Console.WriteLine("3. Import keys");
+                Console.WriteLine("4. Sign file");
+                Console.WriteLine("5. Verify file signature");
+                Console.WriteLine("6. Sign input");
+                Console.WriteLine("7. Exit");
+                Console.Write("Choose an option: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        dsa.GenerateKeys();
+                        Console.WriteLine("Press any key to return....");
+                        Console.ReadKey();
+                        break;
+
+                    case "2":
+                        ExportKeys(dsa);
+                        break;
+
+                    case "3":
+                        ImportKeys(dsa);
+                        break;
+
+                    case "4":
+                        SignFile(dsa);
+                        break;
+
+                    case "5":
+                        Verify(dsa);
+                        break;
+
+                    case "6":
+                       SignInput(dsa);
+                        break;
+
+                    case "7":
+                        Console.WriteLine("Closing the program.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid option. Try again.");
+                        break;
+                }
+            }
+        }
+
+        private static void ImportKeys(DSA dsa)
+        {
+            Console.Write("Enter file path to private key: ");
+            string privateKeyPath = Console.ReadLine();
+            Console.Write("Enter file path to public key: ");
+            string publicKeyPath = Console.ReadLine();
+            dsa.ImportKeys(privateKeyPath, publicKeyPath);
+
+            Console.WriteLine("Press any key to return....");
+            Console.ReadKey();
+        }
+
+        private static void ExportKeys(DSA dsa)
+        {
+            Console.Write("Enter file name to save private key: ");
+            string privateKeyPath = Console.ReadLine();
+            Console.Write("Enter file name to save public key: ");
+            string publicKeyPath = Console.ReadLine();
+            dsa.ExportKeys(privateKeyPath, publicKeyPath);
+
+            Console.WriteLine("Press any key to return....");
+            Console.ReadKey();
+        }
+
+        private static void SignFile(DSA dsa)
+        {
+            Console.Write("Enter file path to sign: ");
+            string filePath = Console.ReadLine();
+            if (File.Exists(filePath))
+            {
+                byte[] signature = dsa.SignFile(filePath);
+                Console.WriteLine($"Sign: {BitConverter.ToString(signature).Replace("-", "")}");
+                dsa.SaveSignature(filePath + ".sig", signature);
+                Console.WriteLine($"Sign saved into file: {filePath}.sig");
+            }
+            else
+            {
+                Console.WriteLine("File doesnt exist.");
+            }
+
+            Console.WriteLine("Press any key to return....");
+            Console.ReadKey();
+        }
+
+        private static void SignInput(DSA dsa)
+        {
+            Console.Write("Enter text to sign: ");
+            string input = Console.ReadLine();
+            byte[] signature = dsa.SignString(input);
+            Console.WriteLine($"Sign: {BitConverter.ToString(signature).Replace("-", "")}");
+
+            Console.WriteLine("Press any key to return....");
+            Console.ReadKey();
+        }
+
+        private static void Verify(DSA dsa)
+        {
+            Console.Write("Enter file path to verify: ");
+            string filePath = Console.ReadLine();
+            Console.Write("Enter file path to signature file: ");
+            string signaturePath = Console.ReadLine();
+            if (File.Exists(filePath) && File.Exists(signaturePath))
+            {
+                byte[] signature = dsa.LoadSignature(signaturePath);
+                bool isVerified = dsa.VerifyFile(filePath, signature);
+                Console.WriteLine($"Signature verification result: {(isVerified ? "Success" : "Failure")}");
+            }
+            else
+            {
+                Console.WriteLine("File or signature doesnt exist.");
+            }
+
+            Console.WriteLine("Press any key to return....");
+            Console.ReadKey();
+        }
+    }
+}
